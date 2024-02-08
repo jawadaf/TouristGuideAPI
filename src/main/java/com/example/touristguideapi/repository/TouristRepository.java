1582fc5ea810129387e68b7fcc539d34fdc88877
jawadaf <jafa0001@stud.kea.dk>
2024-02-08 11:26:23 +0100

    tgAPI

--- src/main/java/com/example/touristguideapi/repository/TouristRepository.java
+++ src/main/java/com/example/touristguideapi/repository/TouristRepository.java
@@ -4,21 +4,22 @@ import com.example.touristguideapi.model.TouristAttraction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class TouristRepository {
 
-    public ResponseEntity<ArrayList<TouristAttraction>> data() {
-        ArrayList<TouristAttraction> data = new ArrayList<>();
+    public ResponseEntity<List<TouristAttraction>> data() {
+        List<TouristAttraction> data = new ArrayList<>();
         data.add(new TouristAttraction("Java", "Programming"));
         data.add(new TouristAttraction("Restaurant", "Food"));
         data.add(new TouristAttraction("Jacket", "Warm"));
         return new ResponseEntity<>(data, HttpStatus.OK);
-
     }
+
+
 
 
 
--- src/main/java/com/example/touristguideapi/service/TouristService.java
+++ src/main/java/com/example/touristguideapi/service/TouristService.java
@@ -3,11 +3,17 @@ package com.example.touristguideapi.service;
 import com.example.touristguideapi.repository.TouristRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class TouristService {
     private TouristRepository touristRepository;
 
     public TouristService(TouristRepository touristRepository) {
         this.touristRepository = new TouristRepository();
+    }
+
+    public {
+
     }
 }
